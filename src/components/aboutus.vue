f0f986f3c334b6fcce1f35c7eb92efc5b119b0a9
ManonMigout <120374212+ManonMigout@users.noreply.github.com>
2023-10-23 14:16:24 +0200

    added the right vue pages

---
+++ src/components/aboutus.vue
@@ -0,0 +1,8 @@
+// aboutus.js
+new Vue({
+    el: '#app',
+    data: {
+        pageTitle: "À propos de nous",
+        pageContent: "We are a french team of two students who want to make the world a greener place! ^^"
+    }
+});
---
+++ src/components/calcul.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div>
+      <h1>{{ pageTitle }}</h1>
+      <p>{{ pageContent }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        pageTitle: "Calcul",
+        pageContent: "Here, you can use our simulator to estimate your carbon footprint and find solutions to lower it!",
+      };
+    },
+  };
+  </script>
+  
